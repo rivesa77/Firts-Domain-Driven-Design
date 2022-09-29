35b9cd9d4dcce8d14e346f5d1c6c37afccac67ee
Ricardo Vela <108927813+rivesa77@users.noreply.github.com>
2022-09-29 13:07:14 +0200

    Cambios Validaciones Libro

--- Libreria.Application/Features/Libros/Commands/Create/CreateLibroCommandValidator.cs
+++ Libreria.Application/Features/Libros/Commands/Create/CreateLibroCommandValidator.cs
@@ -6,8 +6,14 @@ namespace Libreria.Application.Features.Libros.Commands.Create
     {
         public CreateLibroCommandValidator()
         {
-            RuleFor(p => p.Nombre)
+            RuleFor(p => p.Titulo)
                 .NotNull().WithMessage("{Nombre} no puede ser nulo");
+
+            RuleFor(p => p.Asin)
+                .NotNull().WithMessage("{asin} no puede ser nulo");
+
+            RuleFor(p => p.Paginas)                                
+                .GreaterThan(0).WithMessage("{paginas} no puede ser 0");
 
 
         }
