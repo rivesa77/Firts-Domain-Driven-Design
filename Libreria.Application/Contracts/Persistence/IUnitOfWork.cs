2160b5671cf3ae94d6703758ab85356976720aa8
Ricardo Vela <108927813+rivesa77@users.noreply.github.com>
2022-09-29 10:41:04 +0200

    Añadimos el Interfaz de IUnitOfWork

---
+++ Libreria.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Libreria.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Application.Contracts.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+        ILibroRepository LibroRepository { get; }
+        IAutorRepository AutorRepository { get; }
+        IGeneroRepository GeneroRepository { get; }
+        IEditorialRepository EditorialRepository { get; }
+
+        IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : BaseDomainModel;
+
+        Task<int> Complete();
+    }
+}
