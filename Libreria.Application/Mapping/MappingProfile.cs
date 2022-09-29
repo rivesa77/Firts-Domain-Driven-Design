e04e15ed5af24a939dab1a9e5b869750bc802994
Ricardo Vela <108927813+rivesa77@users.noreply.github.com>
2022-09-29 13:15:57 +0200

    Mapping profile

---
+++ Libreria.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Libreria.Application.Features.Autores.Commands.Create;
+using Libreria.Application.Features.Editoriales.Commands.Create;
+using Libreria.Application.Features.Generos.Commands.Create;
+using Libreria.Application.Features.Libros.Commands.Create;
+using Libreria.Domain;
+
+namespace Libreria.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeo de entidad
+            //CreateMap<Libro, LibroVM>();
+            // Mapeo de command
+            CreateMap<CreateLibroCommand, Libro>();
+            CreateMap<CreateGeneroCommand, Genero>();
+            CreateMap<CreateEditorialCommand, Editorial>();
+            CreateMap<CreateAutorCommand, Autor>();
+        }
+    }
+}
